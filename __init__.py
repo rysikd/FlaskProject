27144101b604265bbfbd19e8617ed441fadb38f0
rysikd <aunkoop@gmail.com>
2022-03-20 15:41:08 +0900

    test1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-from FlaskProject import db
-
-db.create_db_for_books()
--- db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-from .settings import DATABASE
-
-
-def create_db_for_books():
-    con = sqlite3.connect(DATABASE)
